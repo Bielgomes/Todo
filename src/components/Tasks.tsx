834b9d942a7d32efcfa10d2d6605f636e60ded5c
Gabriel Gomes da Silva <72311029+Bielgomes@users.noreply.github.com>
2022-07-10 18:31:10 -0300

    Add files via upload

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -4,16 +4,30 @@ import { PlusCircle } from 'phosphor-react';
 
 export function Tasks() {
     return (
-        <div>
-            <header className={styles.header}>
-                <form className={styles.wrapper}>
-                    <input type="text" placeholder="Adicione uma nova tarefa" required />
-                    <button type="submit">
-                        <span>Criar</span>
-                        <PlusCircle size={16} weight={"bold"}/>
-                    </button>
-                </form>
-            </header>
+        <div className={styles.header}>
+            <div className={styles.container}>
+                <header>
+                    <form className={styles.wrapper}>
+                        <input type="text" placeholder="Adicione uma nova tarefa" required />
+                        <button type="submit">
+                            <span>Criar</span>
+                            <PlusCircle size={20} weight={"bold"}/>
+                        </button>
+                    </form>
+                </header>
+                <footer>
+                    <div>
+                        <div>
+                            <strong>Tarefas criadas</strong>
+                            <span>0</span>
+                        </div>
+                        <div>
+                            <strong>Concluidas</strong>
+                            <span>0</span>
+                        </div>
+                    </div>
+                </footer>
+            </div>
         </div>
     )
 }
